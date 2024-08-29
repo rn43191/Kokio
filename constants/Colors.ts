d636d31224657afea39b11af814156613f176b7f
Rowdy <mroddy5280@gmail.com>
2024-08-28 23:35:43 -0700

    feat(Colors.tsx): theme based off of shadcn

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,84 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: "#11181C",
+    background: "#fff",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: "#687076",
+    tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
+
+    // background: '#ffffff',
+    foreground: "#09090b",
+
+    card: "#ffffff",
+    cardForeground: "#09090b",
+
+    popover: "#ffffff",
+    popoverForeground: "#09090b",
+
+    primary: "#006FEE",
+    primaryForeground: "#ffffff",
+
+    secondary: "#f4f4f5",
+    secondaryForeground: "#09090b",
+
+    muted: "#f4f4f5",
+    mutedForeground: "#71717a",
+
+    accent: "#f4f4f5",
+    accentForeground: "#09090b",
+
+    destructive: "#ff0000",
+    destructiveForeground: "#ffffff",
+
+    border: "#e4e4e7",
+
+    input: "#e4e4e7",
+
+    ring: "#006FEE",
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: "#ECEDEE",
+    background: "#151718",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
+
+    // background: '#ffffff',
+    foreground: "#09090b",
+
+    card: "#ffffff",
+    cardForeground: "#09090b",
+
+    popover: "#ffffff",
+    popoverForeground: "#09090b",
+
+    primary: "#006FEE",
+    primaryForeground: "#ffffff",
+
+    secondary: "#f4f4f5",
+    secondaryForeground: "#09090b",
+
+    muted: "#f4f4f5",
+    mutedForeground: "#71717a",
+
+    accent: "#f4f4f5",
+    accentForeground: "#09090b",
+
+    destructive: "#ff0000",
+    destructiveForeground: "#ffffff",
+
+    border: "#e4e4e7",
+
+    input: "#e4e4e7",
+
+    ring: "#006FEE",
   },
 };
