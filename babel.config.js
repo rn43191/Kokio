63591b4621debe7612ca88f6a638fd3eba3f637e
ManulParihar <manul.singh.parihar@gmail.com>
2025-10-21 15:18:12 +0530

    Resolved babel error with react-native-worklets

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,7 @@ module.exports = function (api) {
     ],
     plugins: [
       ["@babel/plugin-syntax-import-assertions"],
+      ["react-native-worklets/plugin"],
       [
         "module-resolver",
         {
