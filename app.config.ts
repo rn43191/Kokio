be57bcc228932b36b9aafc1f8df35b04d47db8e1
Viktor <pernjek@gmail.com>
2025-11-10 12:52:29 +0100

    fix associatedDomains

--- app.config.ts
+++ app.config.ts
@@ -40,7 +40,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "app.kokio",
-      associatedDomains: ["webcredentials:kokio.app"],
+      associatedDomains: ["webcredentials:docs.kokio.app"],
       config: {
         usesNonExemptEncryption: false,
       },
